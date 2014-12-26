a2de77412c339ea4b45760dcde8a71bc1a1aa04b
Kinglong Mee <kinglongmee@gmail.com>
2014-12-26 16:58:17 +0800

    gentags.sh: adds ctags/cscope generation scripts
    Signed-off-by: Kinglong Mee <kinglongmee@gmail.com>

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+INSTALL_DIR="/usr/sbin/"
+
+all: install
+install:
+	cp -rf gentags.sh $(INSTALL_DIR)
---
+++ gentags.sh
@@ -0,0 +1,5 @@
+service firewalld stop
+service bluetooth stop
+umount -f /tmp/
+ctags -R
+cscope -Rkbq
