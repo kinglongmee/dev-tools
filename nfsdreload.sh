ca9a37741205502707c3737e9b7c8e77cded4bc0
Kinglong Mee <kinglongmee@gmail.com>
2015-03-28 23:07:12 +0800

    nfsdreload.sh: make sure stopping nfs service success
    Signed-off-by: Kinglong Mee <kinglongmee@gmail.com>

--- nfsdreload.sh
+++ nfsdreload.sh
@@ -1,4 +1,10 @@
 service nfs stop
-umount /proc/fs/nfsd/
+sleep 1s
+umount -f /proc/fs/nfsd/
+sleep 1s
 lsmod | grep nfsd
 rmmod nfsd
+lsmod | grep nfsd
+cp -f fs/nfsd/nfsd.ko /lib/modules/`uname -r`/kernel/fs/nfsd/nfsd.ko
+service nfs start
+service nfs status
