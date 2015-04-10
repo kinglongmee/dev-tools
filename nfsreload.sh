970fc0cefdde4afe9b9b4f2b0bc62c6e9c815210
Kinglong Mee <kinglongmee@gmail.com>
2015-04-10 19:55:45 +0800

    nfsreload.sh: install fscache.ko
    Signed-off-by: Kinglong Mee <kinglongmee@gmail.com>

--- nfsreload.sh
+++ nfsreload.sh
@@ -1,7 +1,8 @@
-rmmod nfs_layout_nfsv41_files nfs_layout_flexfiles blocklayoutdriver nfsv4 nfs
+rmmod nfs_layout_nfsv41_files nfs_layout_flexfiles blocklayoutdriver nfsv4 nfs fscache
 lsmod | grep nfs
 cp -f fs/nfs/blocklayout/blocklayoutdriver.ko /lib/modules/`uname -r`/kernel/fs/nfs/blocklayout/
 cp -f fs/nfs/filelayout/nfs_layout_nfsv41_files.ko /lib/modules/`uname -r`/kernel/fs/nfs/filelayout/
 cp -f fs/nfs/flexfilelayout/nfs_layout_flexfiles.ko /lib/modules/`uname -r`/kernel/fs/nfs/flexfilelayout/
 cp -f fs/nfs/nfsv4.ko /lib/modules/`uname -r`/kernel/fs/nfs/
 cp -f fs/nfs/nfs.ko /lib/modules/`uname -r`/kernel/fs/nfs/
+cp -f fs/fscache/fscache.ko /lib/modules/`uname -r`/kernel/fs/fscache/
