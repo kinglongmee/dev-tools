6a21c8b914b287958ec6d1e5d9b49713c3ce9431
Kinglong Mee <kinglongmee@gmail.com>
2015-05-26 20:44:09 +0800

    nfsdreload.sh: add coping sunrpc.ko
    Signed-off-by: Kinglong Mee <kinglongmee@gmail.com>

--- nfsdreload.sh
+++ nfsdreload.sh
@@ -6,6 +6,7 @@ lsmod | grep nfsd
 rmmod nfsd
 lsmod | grep nfsd
 cp -f fs/nfsd/nfsd.ko /lib/modules/`uname -r`/kernel/fs/nfsd/nfsd.ko
+cp -f net/sunrpc/sunrpc.ko /lib/modules/`uname -r`/kernel/net/sunrpc/sunrpc.ko
 service rpcbind start
 sleep 1s
 service nfs start
