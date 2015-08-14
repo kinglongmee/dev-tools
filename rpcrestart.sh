a841a242356e47a4e16fe992222c94dfce76daf5
Kinglong Mee <kinglongmee@gmail.com>
2015-08-14 15:36:20 +0800

    New helper of rpcrestart.sh
    Signed-off-by: Kinglong Mee <kinglongmee@gmail.com>

---
+++ rpcrestart.sh
@@ -0,0 +1,8 @@
+service nfs stop
+umount -f /proc/fs/nfsd/
+rmmod nfsd auth_rpcgss nfsv4 blocklayoutdriver nfs lockd nfs_acl
+umount /var/lib/nfs/rpc_pipefs/
+rmmod sunrpc
+lsmod | grep rpc
+cp -f net/sunrpc/sunrpc.ko /lib/modules/`uname -r`/kernel/net/sunrpc/sunrpc.ko
+service rpcbind start
