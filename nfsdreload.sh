b77b58f1418838180fa840d899664ace62f5989a
Kinglong Mee <kinglongmee@gmail.com>
2015-08-14 15:35:52 +0800

    nfsdreload.sh: add updating lockd.ko
    Signed-off-by: Kinglong Mee <kinglongmee@gmail.com>

--- nfsdreload.sh
+++ nfsdreload.sh
@@ -5,6 +5,7 @@ sleep 1s
 lsmod | grep nfsd
 rmmod nfsd
 lsmod | grep nfsd
+cp -f fs/lockd/lockd.ko /lib/modules/`uname -r`/kernel/fs/lockd/lockd.ko
 cp -f fs/nfsd/nfsd.ko /lib/modules/`uname -r`/kernel/fs/nfsd/nfsd.ko
 cp -f net/sunrpc/sunrpc.ko /lib/modules/`uname -r`/kernel/net/sunrpc/sunrpc.ko
 service rpcbind start
